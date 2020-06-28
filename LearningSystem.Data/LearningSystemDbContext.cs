6a8bbce9cf5c1a8b56d08aa0c8acd4ef811f9de7
yavorvasilev <yavor.vasilev666@abv.bg>
2020-06-28 16:32:22 +0300

    Data models migration.

--- LearningSystem.Data/LearningSystemDbContext.cs
+++ LearningSystem.Data/LearningSystemDbContext.cs
@@ -11,8 +11,41 @@
         {
         }
 
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Article> Articles { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder
+                .Entity<StudentCourse>()
+                .HasKey(st => new { st.CourseId, st.StudentId });
+
+            builder
+                .Entity<StudentCourse>()
+                .HasOne(sc => sc.Student)
+                .WithMany(s => s.Courses)
+                .HasForeignKey(sc => sc.StudentId);
+
+            builder
+                .Entity<StudentCourse>()
+                .HasOne(sc => sc.Course)
+                .WithMany(s => s.Students)
+                .HasForeignKey(sc => sc.CourseId);
+
+            builder
+                .Entity<Course>()
+                .HasOne(c => c.Trainer)
+                .WithMany(u => u.Trainings)
+                .HasForeignKey(c => c.TrainerId);
+
+            builder
+                .Entity<Article>()
+                .HasOne(a => a.Author)
+                .WithMany(u => u.Articles)
+                .HasForeignKey(a => a.AuthorId);
+
+
             base.OnModelCreating(builder);
         }
     }
--- LearningSystem.Data/Migrations/20200628130429_InitialTables.Designer.cs
+++ LearningSystem.Data/Migrations/20200628130429_InitialTables.Designer.cs
@@ -4,12 +4,14 @@
     using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.Infrastructure;
     using Microsoft.EntityFrameworkCore.Metadata;
+    using Microsoft.EntityFrameworkCore.Migrations;
     using System;
 
     [DbContext(typeof(LearningSystemDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200628130429_InitialTables")]
+    partial class InitialTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
